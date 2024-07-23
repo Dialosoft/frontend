6cd393135c46bc363a6c0d7f00ed1fff71bf1e7c
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-23 10:37:27 -0500

    feat: add header component to layout.tsx

--- main/src/app/layout.tsx
+++ main/src/app/layout.tsx
@@ -1,14 +1,18 @@
+import dynamic from "next/dynamic";
+
 /* FONT */
 import { Poppins } from "next/font/google";
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
 
 /* Components */
 import "@/app/globals.css";
+const Header = dynamic(() => import("@/components/header/main"));
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
 	return (
 		<html lang="en">
-			<body className={`${poppins.className} antialiased min-h-screen flex`}>
+			<body className={`${poppins.className} antialiased min-h-screen flex flex-col`}>
+				<Header />
 				<main className="flex-grow">{children}</main>
 			</body>
 		</html>
