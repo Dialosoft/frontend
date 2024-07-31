63e1d97cb112a2786db3d8a4fac67789c37fea54
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-31 13:47:14 -0500

    feat: add Zod validation to form

--- main/src/components/Session/Register/form.tsx
+++ main/src/components/Session/Register/form.tsx
@@ -1,54 +1,77 @@
 "use client";
 
-import { useCallback, useState } from "react";
 import debounce from "just-debounce-it";
+import { useCallback, useState } from "react";
+
+import registerSchema from "@/schemas/Session/register";
 
 export default function Register_Form() {
 	const [username, setUsername] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
+	const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
 	/* Username */
-	const debounced_setUsername = useCallback(debounce((value: string) => setUsername(value), 150), []);
+	const debounced_setUsername = useCallback(debounce((value: string) => setUsername(value), 100), []);
 	const handle_Username_Change = (e: React.ChangeEvent<HTMLInputElement>) => debounced_setUsername(e.target.value);
 
 	/* Email */
-	const debounced_setEmail = useCallback(debounce((value: string) => setEmail(value), 150), []);
+	const debounced_setEmail = useCallback(debounce((value: string) => setEmail(value), 100), []);
 	const handle_Email_Change = (e: React.ChangeEvent<HTMLInputElement>) => debounced_setEmail(e.target.value);
 
 	const handleSubmit = (event: React.FormEvent) => {
 		event.preventDefault();
-		console.log({ username, email, password, confirmPassword });
+
+		const result = registerSchema.safeParse({ username, email, password, confirmPassword });
+		if (result.success) {
+			console.log(result.data, "VALID");
+		} else {
+			const fieldErrors: { [key: string]: string } = {};
+
+			result.error.errors.forEach((error) => {
+				if (error.path.length) {
+					fieldErrors[error.path[0]] = error.message;
+				}
+			});
+
+			setErrors(fieldErrors);
+		}
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			{/* Username */}
-			<div className="flex flex-col">
-				<label className="select-none" htmlFor="username">Username</label>
-				<input type="text" value={username} id="username" autoComplete="username" onChange={handle_Username_Change} required />
+		<form onSubmit={handleSubmit} className="flex flex-col items-center justify-center space-y-[2rem]">
+			<div className="flex flex-col items-center justify-center space-y-[1rem]">
+				{/* Username */}
+				<div className="flex flex-col">
+					{errors.username && <span>{errors.username}</span>}
+					<label className="select-none" htmlFor="username">Username</label>
+					<input className="appearance-none" type="text" value={username} id="username" autoComplete="username" onChange={handle_Username_Change} required />
+				</div>
+
+				{/* Email */}
+				<div className="flex flex-col">
+					{errors.email && <span>{errors.email}</span>}
+					<label className="select-none" htmlFor="email">Email</label>
+					<input className="appearance-none" type="email" value={email} id="email" autoComplete="email" onChange={handle_Email_Change} required />
+				</div>
+
+				{/* Password */}
+				<div className="flex flex-col">
+					{errors.password && <span>{errors.password}</span>}
+					<label className="select-none" htmlFor="password">Password</label>
+					<input className="appearance-none" type="password" value={password} id="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} required />
+				</div>
+
+				{/* Confirm Password */}
+				<div className="flex flex-col">
+					{errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+					<label className="select-none" htmlFor="confirm-password">Confirm Password</label>
+					<input className="appearance-none" type="password" value={confirmPassword} id="confirm-password" autoComplete="current-password" onChange={(e) => setConfirmPassword(e.target.value)} required />
+				</div>
 			</div>
 
-			{/* Email */}
-			<div className="flex flex-col">
-				<label className="select-none" htmlFor="email">Email</label>
-				<input type="email" value={email} id="email" autoComplete="email" onChange={handle_Email_Change} required />
-			</div>
-
-			{/* Password */}
-			<div className="flex flex-col">
-				<label className="select-none" htmlFor="password">Password</label>
-				<input type="password" value={password} id="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} required />
-			</div>
-
-			{/* Confirm Password */}
-			<div className="flex flex-col">
-				<label className="select-none" htmlFor="confirm-password">Confirm Password</label>
-				<input type="password" value={confirmPassword} id="confirm-password" onChange={(e) => setConfirmPassword(e.target.value)} required />
-			</div>
-
-			<button type="submit">
+			<button type="submit" disabled={!errors}>
 				<span className="select-none">Register</span>
 			</button>
 		</form>
