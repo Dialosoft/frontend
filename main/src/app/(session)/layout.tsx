864bf1e73a3f7791bf631015f7fcff179c6a3876
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-31 12:54:33 -0500

    feat(layout/session): add metadata to layout session group
    - Added title template.
    - Removed indexing by robots.
    - Added forum category.

--- main/src/app/(session)/layout.tsx
+++ main/src/app/(session)/layout.tsx
@@ -1,3 +1,22 @@
+/* Variables */
+const SITE = process.env.HOST as string;
+
+/* SEO */
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+	metadataBase: new URL(SITE),
+	title: {
+		template: "%s | " + process.env.Name,
+		default: "Session - " + process.env.Name
+	},
+	category: "forum",
+	robots: {
+		index: false,
+		follow: false,
+		nocache: false
+	}
+};
+
 /* FONT */
 import { Poppins } from "next/font/google";
 const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] });
