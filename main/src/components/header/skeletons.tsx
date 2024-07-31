0acf614a91d8654f340d32e27c04b4c0f18bdd60
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-07-31 09:27:52 -0500

    style: add skeletons for suspense input search
    Added loading skeletons for the suspense input search component to enhance user experience during data fetching.

---
+++ main/src/components/header/skeletons.tsx
@@ -0,0 +1,6 @@
+export const Search_sk = () => (
+	<div className="w-1/3 animate-pulse bg-black-500 bg-opacity-25 rounded-full px-[1rem] py-[.8rem] flex items-center justify-start space-x-[1rem]">
+		<div className="w-[1.5rem] h-[1.5rem] rounded-full bg-black-500 bg-opacity-40"></div>
+		<div className="w-full h-[1.2rem] rounded-full bg-black-500 bg-opacity-40"></div>
+	</div>
+);
