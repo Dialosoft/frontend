86769381fd85efdbd96694c167090a8772fc24c9
Flavio Taccarelli <flaviotacca@hotmail.com>
2024-08-06 18:50:33 +0200

    style: update primary color

--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -11,23 +11,23 @@ const config: Config = {
 		extend: {
 			colors: {
 				primary: {
-					50: "#f0fdf3",
-					100: "#dcfce4",
-					200: "#bcf6cb",
-					300: "#87eea3",
-					400: "#4cdc74",
-					500: "#27d257",
-					600: "#18a13e",
-					700: "#167f34",
-					800: "#17642d",
-					900: "#155228",
-					950: "#062d13"
+					50: "#eefbfd",
+					100: "#d4f4f9",
+					200: "#aee9f3",
+					300: "#76d8ea",
+					400: "#27b4d2",
+					500: "#1aa0c0",
+					600: "#1981a1",
+					700: "#1b6983",
+					800: "#1f566b",
+					900: "#1e485b",
+					950: "#0e2e3e",
 				},
 				secondary: "#D3D3ED",
 				black: {
 					500: "#3A3A50",
-					900: "#0F0F13"
-				}
+					900: "#0F0F13",
+				},
 			},
 			screens: {
 				sm: "100%",
@@ -36,20 +36,20 @@ const config: Config = {
 				xl: "1280px",
 				"2xl": "1536px",
 				"3xl": "1920px",
-				"4xl": "2560px"
+				"4xl": "2560px",
 			},
 			container: {
 				center: true,
 				padding: {
-					DEFAULT: "1rem"
+					DEFAULT: "1rem",
 				},
 				screens: {
-					"3xl": "1920px"
-				}
-			}
-		}
+					"3xl": "1920px",
+				},
+			},
+		},
 	},
-	plugins: []
+	plugins: [],
 };
 
 export default config;
