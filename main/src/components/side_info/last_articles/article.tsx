308ab9b4c8e0df4fdd6091617559d6070ca4a3b3
Busta <josuebustatr@gmail.com>
2024-08-06 19:31:52 +0200

    feat: article componente created

---
+++ main/src/components/side_info/last_articles/article.tsx
@@ -0,0 +1,22 @@
+interface ArticleProps {
+	title: string;
+	user: string;
+	time: string;
+}
+
+export default function Article({ title, user, time }: ArticleProps) {
+	return (
+		<>
+			<div className="flex space-x-2 items-center hover:bg-black-500 p-2 rounded-sm hover:bg-opacity-25">
+				<div className="border border-primary-500  rounded-full aspect-square h-12  " />
+				<div className=" w-full">
+					<h3>{title}</h3>
+					<div className="flex text-xs w-full justify-between text-black-500 ">
+						<div>{user}</div>
+						<div>{time}</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
