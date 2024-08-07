489adaa6ebe4132b5c4c1e9006eaff38d2d51630
Busta <josuebustatr@gmail.com>
2024-08-07 15:28:58 +0200

    fix: index removed and uuid added

--- main/src/components/side_info/members/main.tsx
+++ main/src/components/side_info/members/main.tsx
@@ -1,5 +1,5 @@
 import Member from "./member";
-
+import { v4 as uuidv4 } from "uuid";
 export default function Members() {
 	const member = [
 		{ name: "Alejandro", username: "@alejandro", rol: "Admin", photo: "W" },
@@ -11,8 +11,8 @@ export default function Members() {
 		<>
 			<h2 className=" text-3xl font-semibold">Last Articles</h2>
 			<div className="bg-black-500 bg-opacity-25 max-w-[317px] p-2 space-y-2 rounded-lg">
-				{member.map((member, index) => (
-					<Member						key={index}
+				{member.map((member) => (
+					<Member						key={uuidv4()}
 						name={member.name}
 						username={member.username}
 						rol={member.rol}
