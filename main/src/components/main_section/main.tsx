b5d2eaec95979eec0c72feeeba92cff6054079f9
Busta <josuebustatr@gmail.com>
2024-08-07 23:26:51 +0200

    fix: Link from main to category

--- main/src/components/main_section/main.tsx
+++ main/src/components/main_section/main.tsx
@@ -1,20 +1,24 @@
 import Category from "./category";
 import { v4 as uuidv4 } from "uuid";
+import Link from "next/link";
 export default function MainSection() {
 	const category = [
 		{
+			id: "1",
 			type: "Info",
 			title: "News and announcements",
 			posts: 52,
 			comments: 2133,
 		},
 		{
+			id: "2",
 			type: "Event",
 			title: "Promotions & events",
 			posts: 32,
 			comments: 8333,
 		},
 		{
+			id: "3",
 			type: "Help",
 			title: "Rules & FAQs",
 			posts: 62,
@@ -27,13 +31,14 @@ export default function MainSection() {
 			<h2 className=" text-3xl font-semibold">Main Category</h2>
 			<div className="bg-black-500 bg-opacity-25 max-w-[1110px] p-2 space-y-2 rounded-lg">
 				{category.map(category => (
-					<Category
-						key={uuidv4()}
-						type={category.type}
-						title={category.title}
-						posts={category.posts}
-						comments={category.comments}
-					/>
+					<Link href={`c/${category.id}`} key={uuidv4()}>
+						<Category
+							type={category.type}
+							title={category.title}
+							posts={category.posts}
+							comments={category.comments}
+						/>
+					</Link>
 				))}
 			</div>
 		</div>
