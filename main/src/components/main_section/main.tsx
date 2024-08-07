a5bb8c0adef71726cd1d8e8a58bfd983c47de067
Busta <josuebustatr@gmail.com>
2024-08-07 14:49:12 +0200

    feat/ main categories created

---
+++ main/src/components/main_section/main.tsx
@@ -0,0 +1,40 @@
+import Category from "./category";
+export default function MainSection() {
+	const category = [
+		{
+			type: "Info",
+			title: " News and announcements",
+			posts: 32,
+			comments: 2333,
+		},
+		{
+			type: "Event",
+			title: " News and announcements",
+			posts: 32,
+			comments: 2333,
+		},
+		{
+			type: "Help",
+			title: " News and announcements",
+			posts: 32,
+			comments: 2333,
+		},
+	];
+
+	return (
+		<div className="w-full space-y-4">
+			<h2 className=" text-3xl font-semibold">Main Category</h2>
+			<div className="bg-black-500 bg-opacity-25 max-w-[1110px] p-2 space-y-2 rounded-lg">
+				{category.map((category, index) => (
+					<Category
+						key={index}
+						type={category.type}
+						title={category.title}
+						posts={category.posts}
+						comments={category.comments}
+					/>
+				))}
+			</div>
+		</div>
+	);
+}
