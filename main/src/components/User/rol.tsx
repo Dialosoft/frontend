721802794050810d46f181b0fbd9a9736e9354d2
Busta <josuebustatr@gmail.com>
2024-08-08 22:51:45 +0200

    fix: rol icons and size changed

--- main/src/components/User/rol.tsx
+++ main/src/components/User/rol.tsx
@@ -1,4 +1,6 @@
 import { Crown } from "lucide-react";
+import { Sword } from "lucide-react";
+import { UserRound } from "lucide-react";
 
 interface RolProps {
 	rol: "Admin" | "Moderator" | "User";
@@ -8,7 +10,7 @@ export default function Rol({ rol }: RolProps) {
 	switch (rol) {
 		case "Admin":
 			return (
-				<div className="flex items-center space-x-1 text-yellow border border-yellow bg-yellow bg-opacity-25 py-1 px-2 rounded-md">
+				<div className="flex items-center space-x-1 text-yellow border h-6 border-yellow bg-yellow bg-opacity-25 py-1 px-2 rounded-md">
 					<Crown className="aspect-square w-4 h-4" />
 
 					<span className="text-xs">{rol}</span>
@@ -16,16 +18,16 @@ export default function Rol({ rol }: RolProps) {
 			);
 		case "Moderator":
 			return (
-				<div className="flex items-center space-x-1 text-green border border-green bg-green bg-opacity-25 py-1 px-2 rounded-md">
-					<Crown className="aspect-square w-4 h-4" />
+				<div className="flex items-center space-x-1 text-green border h-6 border-green bg-green bg-opacity-25 py-1 px-2 rounded-md">
+					<Sword className="aspect-square w-4 h-4" />
 
 					<span className="text-xs">{rol}</span>
 				</div>
 			);
 		case "User":
 			return (
-				<div className="flex items-center space-x-1 text-primary-400 border border-primary-400 bg-primary-400 bg-opacity-25 py-1 px-2 rounded-md">
-					<Crown className="aspect-square w-4 h-4" />
+				<div className="flex items-center space-x-1 text-primary-400 border h-6 border-primary-400 bg-primary-400 bg-opacity-25 py-1 px-2 rounded-md">
+					<UserRound className="aspect-square w-4 h-4" />
 
 					<span className="text-xs">{rol}</span>
 				</div>
