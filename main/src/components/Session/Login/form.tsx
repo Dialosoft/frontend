b53037567dd290be17882e7b3287a87dc5686fc6
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-12 10:46:08 -0500

    style(login): add styles to login form

--- main/src/components/Session/Login/form.tsx
+++ main/src/components/Session/Login/form.tsx
@@ -11,10 +11,7 @@ export default function Login_Form() {
 	const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
 	/* Username or Email */
-	const debounced_setUserOrEmail = useCallback(
-		debounce((value: string) => setUserOrEmail(value), 50),
-		[setUserOrEmail]
-	);
+	const debounced_setUserOrEmail = useCallback(debounce((value: string) => setUserOrEmail(value), 50),[setUserOrEmail]);
 	const handle_UserOrEmail_Change = (e: React.ChangeEvent<HTMLInputElement>) => debounced_setUserOrEmail(e.target.value);
 
 	const handleSubmit = (event: React.FormEvent) => {
@@ -37,21 +34,27 @@ export default function Login_Form() {
 	};
 
 	return (
-		<form onSubmit={handleSubmit} className="flex flex-col items-center justify-center space-y-[2rem]">
-			<div className="flex flex-col items-center justify-center space-y-[1rem]">
-				<div className="flex flex-col">
-					<label className="select-none" htmlFor="UsernameOrEmail">Email or Username</label>
-					<input className="appearance-none" type="text" value={UserOrEmail} id="UsernameOrEmail" autoComplete="username" onChange={handle_UserOrEmail_Change} minLength={1} maxLength={254} required />
+		<form onSubmit={handleSubmit} className="w-[20rem] flex flex-col items-center justify-center space-y-[2rem]">
+			<div className="w-full flex flex-col items-center justify-center space-y-[1rem]">
+				<div className="w-full flex flex-col space-y-[.2rem]">
+					<div className="flex items-center justify-between">
+						<label className="select-none font-medium" htmlFor="UsernameOrEmail">Email or Username</label>
+						{errors.UserOrEmail && <span className="text-red">{errors.UserOrEmail}</span>}
+					</div>
+					<input className="appearance-none focus:outline-none bg-black-300 bg-opacity-25 border border-black-300 rounded-md px-[.6rem] py-[.4rem]" type="text" value={UserOrEmail} id="UsernameOrEmail" autoComplete="username" onChange={handle_UserOrEmail_Change} minLength={1} maxLength={254} required />
 				</div>
 
-				<div className="flex flex-col">
-					<label className="select-none" htmlFor="password">Password</label>
-					<input className="appearance-none" type="password" value={password} id="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} minLength={8} maxLength={50} required />
+				<div className="w-full flex flex-col space-y-[.2rem]">
+					<div className="flex items-center justify-between">
+						<label className="select-none font-medium" htmlFor="password">Password</label>
+						{errors.password && <span className="text-red">{errors.password}</span>}
+					</div>
+					<input className={`appearance-none focus:outline-none bg-black-300 bg-opacity-25 border border-black-300 rounded-md px-[.6rem] py-[.4rem] ${errors.password && 'border-red'}`} type="password" value={password} id="password" autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} minLength={8} maxLength={50} required />
 				</div>
 			</div>
 
-			<button type="submit" disabled={!!Object.keys(errors).length}>
-				<span className="select-none">Login</span>
+			<button className="w-full bg-primary-400 rounded-md py-[.4rem] group disabled:bg-black-300" type="submit" disabled={!!Object.keys(errors).length}>
+				<span className="select-none text-black-900 group-disabled:text-secondary">Login</span>
 			</button>
 		</form>
 	);
