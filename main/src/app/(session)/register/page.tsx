f9c3634586e268cb8ddca3dbfe1aaf2b8347ba15
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-12 08:53:33 -0500

    style(register): increase title size on register page

--- main/src/app/(session)/register/page.tsx
+++ main/src/app/(session)/register/page.tsx
@@ -12,7 +12,7 @@ export default function Register() {
 	return (
 		<div className="h-full flex flex-col items-center justify-center space-y-[4rem]">
 			<div>
-				<h1 className="select-none font-medium text-[3rem] xl:text-[5rem]">{process.env.Name}</h1>
+				<h1 className="select-none font-medium text-[4rem] xl:text-[6rem]">{process.env.Name}</h1>
 			</div>
 
 			<Form />
