4ab20f6b7dfd2a451e41d2bab9788d5cf7d0b278
Busta <josuebustatr@gmail.com>
2024-08-12 21:43:43 +0200

    feat: settings section sidebar implemented

--- main/src/app/(forum)/(account)/a/page.tsx
+++ main/src/app/(forum)/(account)/a/page.tsx
@@ -1,6 +1,21 @@
-export default function Account() {
-	return(
-		<>
-		</>
-	)
+"use client";
+import Profile from "@/components/Account/Profile_Section/profile";
+import { useState } from "react";
+import AccountSideNav from "@/components/Account/sidenav";
+export default function SettingsSection() {
+	const UserInfo = {
+		feed: 21,
+		posts: 32,
+		answers: 223,
+	};
+
+	const [activeSection, setActiveSection] = useState("feed");
+
+	return (
+		<div className="container   flex  space-x-4 mt-16">
+			<div className="max-w-[317px] w-full">
+				<AccountSideNav />
+			</div>
+		</div>
+	);
 }
