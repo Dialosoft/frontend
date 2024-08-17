03e071969039fc56c2a8d652d308fe1796374de0
Busta <josuebustatr@gmail.com>
2024-08-17 16:52:44 +0200

    feat: profile compo completed

--- main/src/components/Account/Profile_Section/profile.tsx
+++ main/src/components/Account/Profile_Section/profile.tsx
@@ -1,36 +1,37 @@
 import { Settings } from "lucide-react";
 import Link from "next/link";
 import Rol from "@/components/User/rol";
-export default function Profile() {
-	const User = {
-		name: "Alejandro",
-		username: "@alejandro",
-		role: "Admin",
-		pronoun: "she/her",
-		registration_date: "28 February 2020",
-		answers: 3413,
-		likes: 3232,
-		best_answers: 323,
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae scelerisque tortor. Maecenas aliquet libero vitae nisl lobortis.",
-	};
+interface ProfileProps{
+	name: string,
+		username:string,
+		role: string,
+		pronoun: string,
+		registration_date: string,
+		answers: number,
+		
+		likes: number,
+		best_answers: number,
+		description: string
+}
+export default function Profile({name, username, role, pronoun, registration_date, answers, likes, best_answers, description}:ProfileProps) {
+	
 	return (
-		<div className="max-w-[778px]  w-full  bg-black-300 bg-opacity-25 space-y-4 rounded-lg p-4 relative">
+		<div className="max-w-[778px]  w-full  bg-black-300 bg-opacity-25 space-y-4 rounded-lg p-4 relative min-h-[264px]">
 			<div className="flex items-center space-x-4 ">
 				<div className="border-2 border-yellow h-36 w-36 bg-black-900 rounded-full"></div>
 				<div className="flex-col space-y-5">
 					<div className="space-y-1">
 						<div className="flex items-center space-x-2">
 							<div className="text-3xl font-semibold">
-								{User.name}
+								{name}
 							</div>
-							<Rol rol={User.role} />
+							<Rol rol={role} />
 						</div>
 						<div className="flex items-center space-x-2 text-black-500">
-							<div>{User.username}</div>
+							<div>{username}</div>
 							<div className="h-1.5 w-1.5 bg-black-500 rounded-full" />
 
-							<div>{User.pronoun}</div>
+							<div>{pronoun}</div>
 						</div>
 					</div>
 
@@ -38,24 +39,24 @@ export default function Profile() {
 						<div className="">
 							<div className="text-black-500">Registration</div>
 							<div className="font-medium">
-								{User.registration_date}
+								{registration_date}
 							</div>
 						</div>
 						<div className="h-4 w-0.5 bg-black-500 rounded-full bg-opacity-25" />
 						<div>
 							<div className="text-black-500">Answers</div>
-							<div className="font-medium">{User.answers}</div>
+							<div className="font-medium">{answers}</div>
 						</div>
 						<div className="h-4 w-0.5 bg-black-500 rounded-full bg-opacity-25" />
 						<div>
 							<div className="text-black-500">Likes Recived</div>
-							<div className="font-medium">{User.likes}</div>
+							<div className="font-medium">{likes}</div>
 						</div>
 						<div className="h-4 w-0.5 bg-black-500 rounded-full bg-opacity-25" />
 						<div>
 							<div className="text-black-500">Best Answers</div>
 							<div className="font-medium">
-								{User.best_answers}
+								{best_answers}
 							</div>
 						</div>
 					</div>
@@ -63,7 +64,7 @@ export default function Profile() {
 			</div>
 			<div>
 				<div>Description</div>
-				<div className="text-black-500">{User.description}</div>
+				<div className="text-black-500">{description}</div>
 			</div>
 			<Link href={"/a/settings/account"}>
 				<Settings className="absolute top-4 right-5 text-black-500 hover:text-secondary cursor-pointer" />
