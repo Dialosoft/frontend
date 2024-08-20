813e2b247cfb890dd3957836d9a6ee87df0a6aec
Busta <josuebustatr@gmail.com>
2024-08-20 18:05:14 +0200

    feat: navs for settings/security implemented

--- main/src/app/(forum)/(account)/a/settings/security/page.tsx
+++ main/src/app/(forum)/(account)/a/settings/security/page.tsx
@@ -1,6 +1,16 @@
-export default function Security() {
-	return(
-		<>
-		</>
-	)
+import SettingsNav from "@/components/Account/Settings_Section/settingsnav";
+import AccountSideNav from "@/components/Account/sidenav";
+
+export default function StgsSecurity() {
+	return (
+		<div className="container   flex  space-x-4 mt-16">
+			<div className="max-w-[317px] w-full">
+				<AccountSideNav />
+			</div>
+			<div className="max-w-[317px] w-full space-y-4">
+				<div className="h-10 rounded-full bg-black-300 bg-opacity-25"></div>
+				<SettingsNav />
+			</div>
+		</div>
+	);
 }
