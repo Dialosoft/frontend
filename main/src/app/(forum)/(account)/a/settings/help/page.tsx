1e18d101eeefe17d9e433a54963b9bf9c4ee552d
Busta <josuebustatr@gmail.com>
2024-08-20 18:04:17 +0200

    feat: page settings/help created

---
+++ main/src/app/(forum)/(account)/a/settings/help/page.tsx
@@ -0,0 +1,16 @@
+import SettingsNav from "@/components/Account/Settings_Section/settingsnav";
+import AccountSideNav from "@/components/Account/sidenav";
+
+export default function StgsHelp() {
+	return (
+		<div className="container   flex  space-x-4 mt-16">
+			<div className="max-w-[317px] w-full">
+				<AccountSideNav />
+			</div>
+			<div className="max-w-[317px] w-full space-y-4">
+				<div className="h-10 rounded-full bg-black-300 bg-opacity-25"></div>
+				<SettingsNav />
+			</div>
+		</div>
+	);
+}
