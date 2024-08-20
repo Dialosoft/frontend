e482cd1394fabd47e8e748ef78f044262111fe58
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-20 09:51:19 -0500

    ✨ feat: add robots.txt file for search engine crawlers

---
+++ main/src/app/robots.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+	return {
+		rules: {
+			userAgent: "*",
+			allow: "/",
+			disallow: [
+				"/login",
+				"/register",
+				"/a"
+			]
+		}
+	}
+}
