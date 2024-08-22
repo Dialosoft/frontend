e73dbaa948537b8ee210f6cc3b6be0b6ee23a7da
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-21 22:20:51 -0500

    feat: add dark mode beta version

--- main/src/components/Forum/Header/User/main.tsx
+++ main/src/components/Forum/Header/User/main.tsx
@@ -17,10 +17,10 @@ interface UserProps {
 }
 
 export default function User({ name, admin, mod }: UserProps) {
-	const [colorTheme, setTheme] = useDarkMode();
+	const [theme, setTheme] = useDarkMode();
 
 	const toggleDark = () => {
-		setTheme(colorTheme);
+		setTheme(theme === "dark" ? "light" : "dark");
 	};
 
 	const menuItems = [
@@ -42,7 +42,7 @@ export default function User({ name, admin, mod }: UserProps) {
 	];
 
 	return (
-		<div className="absolute overflow-hidden w-64 top-full mt-[1rem] right-0 rounded-lg bg-black-700 border border-opacity-25 border-black-300 text-black-500">
+		<div className="absolute overflow-hidden w-72 top-full mt-[1rem] right-0 rounded-lg bg-secondary dark:bg-black-700 border border-opacity-25 border-black-300 text-black-500">
 			<div className="m-[1rem] flex flex-col items-center justify-start space-y-[1rem]">
 				<div className="w-full flex items-center justify-between">
 					<span className="select-none text-secondary font-semibold">{name}</span>
@@ -61,12 +61,16 @@ export default function User({ name, admin, mod }: UserProps) {
 
 					<button className="w-full p-2 rounded-md flex items-center justify-between transition-all ease-in-out duration-300 group hover:bg-black-300 hover:bg-opacity-25" onClick={toggleDark}>
 						<div className="flex items-center justify-start space-x-[.5rem] transition-all ease-in-out duration-300 group-hover:text-secondary">
-							<div>{colorTheme === "light" ? (<Moon size={24} />) : (<Sun size={24} />)}</div>
-							<span className="select-none">{colorTheme === "light" ? "Dark Mode" : "Light Mode"}</span>
+							<div>{theme === "dark" ? (<Moon size={24} />) : (<Sun size={24} />)}</div>
+							<span className="select-none">{theme === "dark" ? "Dark Mode" : "Light Mode"}</span>
+							
+							<div className="text-yellow rounded-md border border-yellow bg-yellow bg-opacity-25 px-1 py-0.5 flex items-center justify-center">
+								<span className="select-none text-xs">Beta</span>
+							</div>
 						</div>
 
 						<div className="bg-black-300 bg-opacity-25 rounded-full h-5 w-10">
-							<div className={`bg-primary-400 rounded-full aspect-square h-full ease-in duration-100 ${(colorTheme === "dark") && "translate-x-5"}`}></div>
+							<div className={`bg-primary-400 rounded-full aspect-square h-full ease-in duration-100 ${(theme === "dark") && "translate-x-5"}`}></div>
 						</div>
 					</button>
 
