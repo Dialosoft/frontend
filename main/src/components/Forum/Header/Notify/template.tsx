bf83a78b41f159bdafa0a2524be08d69b7adee6c
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-21 21:54:27 -0500

    feat(notify): add icon to notification template

--- main/src/components/Forum/Header/Notify/template.tsx
+++ main/src/components/Forum/Header/Notify/template.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { UserRound } from "lucide-react";
 
 interface NotifyProps {
 	link: string,
@@ -14,7 +15,9 @@ export default function Notify_Template({ name, type, time, link }: NotifyProps)
 		<Link className="inline-block w-full px-2 py-1 transition-all ease-in-out duration-300 hover:bg-black-300 hover:bg-opacity-25 rounded-md" title={fullText} href={link} prefetch={false}>
 			<div className="flex items-center justify-between">
 				<div className="flex items-center space-x-[.5rem] overflow-hidden">
-					<div className="w-8 h-8 flex-shrink-0 rounded-full border border-primary-400"></div>
+					<div className="w-8 h-8 flex-shrink-0 rounded-full border border-primary-400 flex items-center justify-center">
+						<UserRound size={20} />
+					</div>
 					<span className="select-none flex-1 truncate text-black-500">
 						<span className="text-secondary">{name}</span> <span>{type}</span>
 					</span>
