9010cdb0dcafe390f2ade1155475e385e1e981dd
Busta <josuebustatr@gmail.com>
2024-08-22 23:40:51 +0200

    style: responsive last post

--- main/src/components/Forum/main_section/last_post.tsx
+++ main/src/components/Forum/main_section/last_post.tsx
@@ -8,7 +8,7 @@ interface PostProps {
 export default function LastPost({ title, user, time }: PostProps) {
 	return (
 		<>
-			<div className="flex space-x-2 items-center   rounded-sm ">
+			<div className="flex space-x-2 items-center max-md:hidden max-w-56  w-56  rounded-sm ">
 				<div className="border border-primary-500  rounded-full aspect-square h-12  " />
 				<div className=" w-full">
 					<h3>{title}</h3>
