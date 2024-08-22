fc9bfd6469d1540fcfc81ab777f2b9d753130280
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-21 21:44:19 -0500

    feat: add delete notifications feature

--- main/src/components/Forum/Header/Notify/main.tsx
+++ main/src/components/Forum/Header/Notify/main.tsx
@@ -1,19 +1,23 @@
 
-import { Suspense } from "react";
 import dynamic from "next/dynamic";
 import { Bell } from "lucide-react";
 import { v4 as uuidv4 } from "uuid";
+import { Suspense, useState } from "react";
 
 const Template = dynamic(() => import("@/components/Forum/Header/Notify/template"));
 
 export default function Notify() {
-	const notifyTemplate = [
+	const [notifyTemplate, setNotifyTemplate] = useState([
 		{ photo: "", name: "SirLikesALot", type: "liked your post... again", time: "1h", link: "" },
 		{ photo: "", name: "CaptainObvious", type: "replied to your thread with more questions", time: "30m", link: "" },
 		{ photo: "", name: "MissClickQueen", type: "accidentally reported your comment", time: "15m", link: "" },
 		{ photo: "", name: "DrGrammar", type: "corrected your grammar in a reply", time: "45m", link: "" },
 		{ photo: "", name: "MrEcho", type: "reposted your exact words in another thread", time: "2h", link: "" }
-	];
+	]);
+
+	const clearNotifications = () => {
+		setNotifyTemplate([]);
+	};
 
 	return (
 		<div className="absolute overflow-hidden w-96 top-full mt-[1rem] right-0 rounded-lg bg-black-700 border border-opacity-25 border-black-300 text-black-500">
@@ -29,10 +33,14 @@ export default function Notify() {
 							<Template name={notify.name} link={notify.link} type={notify.type} time={notify.time} />
 						</Suspense>
 					))}
+					
+					{!notifyTemplate.length && (
+						<span className="flex items-center justify-center select-none py-[1rem]">No notifications</span>
+					)}
 				</div>
 
 				<div className="w-full flex items-center justify-between text-sm">
-					<span className="select-none hover:text-red cursor-pointer">Delete</span>
+					<span onClick={clearNotifications} className="select-none hover:text-red cursor-pointer">Delete</span>
 					<span className="select-none hover:text-secondary cursor-pointer">See More</span>
 				</div>
 			</div>
