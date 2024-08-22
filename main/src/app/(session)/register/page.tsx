9d72621fe1e96ee41acb50e60a1577a90a96cea2
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-21 21:32:45 -0500

    feat: add redirect to home on logo click

--- main/src/app/(session)/register/page.tsx
+++ main/src/app/(session)/register/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 
@@ -12,10 +13,10 @@ const Form = dynamic(() => import("@/components/Session/Register/form"), { ssr: 
 export default function Register() {
 	return (
 		<div className="h-full flex flex-col items-center justify-center space-y-[4rem]">
-			<div className="flex items-center justify-center space-x-[1rem]">
+			<Link href="/" prefetch={false} className="flex items-center justify-center space-x-[1rem]">
 				<Image className="order-1 select-none" src="/logo.png" alt="Forum Logo" width={100} height={100} style={{ width: "100%", height: "auto" }} />
 				<h1 className="order-2 select-none font-medium text-[4rem] xl:text-[6rem]">{process.env.Name}</h1>
-			</div>
+			</Link>
 
 			<Form />
 		</div>
