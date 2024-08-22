0319ac71b32b95ef9b1c7875b6eb46cefb020679
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-22 14:53:34 -0500

    fix: style tailwincss

--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -35,11 +35,6 @@ const config: Config = {
 				yellow: "#D29827",
 			},
 			screens: {
-				sm: "100%",
-				md: "768px",
-				lg: "1024px",
-				xl: "1280px",
-				"2xl": "1536px",
 				"3xl": "1920px",
 				"4xl": "2560px",
 			},
