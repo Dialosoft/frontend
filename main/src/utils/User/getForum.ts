0cbeaf35f9b7cc1d3ecf0ae6c24f24b8cfdbf737
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-22 21:20:15 -0500

    feat: post

---
+++ main/src/utils/User/getForum.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import axios from "axios";
+import { cookies } from "next/headers";
+
+export async function getForums_byUser() {
+	const session = cookies().has("_rtkn");
+	if (!session) return false;
+	
+	const sessionUser = cookies().get("_atkn");
+	if (!sessionUser?.value) return false;
+
+	try {
+		const response = await axios.get("http://gateway-service:8080/dialosoft-api/user-service/get-user-info",
+			{
+				headers: {
+					"Authorization": "Bearer " + sessionUser.value,
+				},
+				timeout: (30 * 1000), // 30 seconds
+			}
+		);
+
+		return response.data.data;
+	} catch (error) {
+		return false;
+	}
+}
