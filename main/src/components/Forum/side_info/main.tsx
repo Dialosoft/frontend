c743f3f39ac208c7c32e603a1827d475da3d8d52
Busta <josuebustatr@gmail.com>
2024-08-23 02:30:10 +0200

    fix: aside lg to xl

--- main/src/components/Forum/side_info/main.tsx
+++ main/src/components/Forum/side_info/main.tsx
@@ -3,7 +3,7 @@ import Members from "./members/main";
 
 export default function Aside() {
 	return (
-		<div className="flex-col space-y-4 max-xl:hidden">
+		<div className="flex-col space-y-4 max-lg:hidden">
 			<LastArticles />
 			<Members />
 		</div>
