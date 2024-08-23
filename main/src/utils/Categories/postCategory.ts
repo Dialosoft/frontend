0688c27df0e20f0331b1a70fca1a208e92faedb8
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-23 09:33:51 -0500

    fix: posts request

--- main/src/utils/Categories/postCategory.ts
+++ main/src/utils/Categories/postCategory.ts
@@ -7,26 +7,26 @@ interface CategoryProps {
 	name: string;
 	description: string;
 }
-export async function PostCategory({ name, description }:CategoryProps) {
+export async function PostCategory({ name, description }: CategoryProps) {
 	const result = categorySchema.safeParse({ name, description });
 	if (!result.success) {
 		return { success: false, message: "Validation failed" };
 	}
-	try{
+	try {
 		const response = await axios.post(
 			"http://gateway-service:8080/dialosoft-api/management-service/create-category",
 			{
-			name: name.toLowerCase(),
-			description: description,
-		},
-			{
-			headers: {
-				"Content-Type": "application/json",
+				name: name.toLowerCase(),
+				description: description,
 			},
-			timeout: 60 * 1000, // 1 minute
-		}
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+				timeout: 60 * 1000, // 1 minute
+			}
+		);
 		return response.data.data;
-
 	} catch {
 		return false;
 	}
