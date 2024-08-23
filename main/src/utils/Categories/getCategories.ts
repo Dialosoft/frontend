441b730b7a043e243a0e5f3bd1384687c34e7901
Jaroc <75870284+Jaro-c@users.noreply.github.com>
2024-08-23 09:28:42 -0500

    fix: remove console

--- main/src/utils/Categories/getCategories.ts
+++ main/src/utils/Categories/getCategories.ts
@@ -6,8 +6,7 @@ export async function getCategories() {
 	try {
 		const response = await axios.get("http://gateway-service:8080/dialosoft-api/management-service/get-all-categories");
 		return response.data.data;
-	} catch (e) {
-		console.log(e.response);
+	} catch {
 		return false;
 	}
 }
